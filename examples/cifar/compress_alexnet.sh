4272f1f1ca035f36c00d5d2bbb5ddaf586bace3f
Saurav Muralidharan <sauravm@nvidia.com>
2019-08-15 16:22:29 -0700

    Add CIFAR-10 example

---
+++ examples/cifar/compress_alexnet.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+
+if [[ $# -eq 0 ]]; then
+  echo "Usage: compress_alexnet.sh [scheme] [density] [#iterations]"
+  exit 1
+fi
+
+SCHEME=${1}
+DENSITY=${2}
+STEPS=${3}
+
+PREFIX=alexnet_${SCHEME}_${DENSITY//[\.]/_}
+
+python compress.py\
+       --arch alexnet --dataset cifar10\
+       --lr 1e-2 --lr_decay 1e-4\
+       --weight_decay 0\
+       --momentum 0.95\
+       --mb_iterations_per_l 3000\
+       --mb_iterations_first_l 30000\
+       --mu_init 1e-3 --mu_multiplier 1.1\
+       --l_batch_size 128\
+       --model trained/alexnet.pth\
+       --scheme ${SCHEME}\
+       --density ${DENSITY}\
+       --out compressed/${PREFIX}.pth\
+       --csv results/${PREFIX}.csv\
+       -v --steps ${STEPS}
+
---
+++ examples/cifar/models/vgg.py
@@ -0,0 +1,108 @@
+# Copyright 2019 NVIDIA Corporation
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from __future__ import absolute_import
+
+import torch.nn as nn
+import math
+
+__all__ = [
+    'vgg11', 'vgg11_bn', 'vgg13', 'vgg13_bn',
+    'vgg16', 'vgg16_bn', 'vgg19_bn', 'vgg19'
+]
+
+class VGG(nn.Module):
+    def __init__(self, features, num_classes=10):
+        super(VGG, self).__init__()
+        self.features = features
+        self.classifier = nn.Linear(512, num_classes)
+        self._initialize_weights()
+
+    def forward(self, x):
+        x = self.features(x)
+        x = x.view(x.size(0), -1)
+        x = self.classifier(x)
+        return x
+
+    def _initialize_weights(self):
+        for m in self.modules():
+            if isinstance(m, nn.Conv2d):
+                n = m.kernel_size[0] * m.kernel_size[1] * m.out_channels
+                m.weight.data.normal_(0, math.sqrt(2. / n))
+                if m.bias is not None:
+                    m.bias.data.zero_()
+            elif isinstance(m, nn.BatchNorm2d):
+                m.weight.data.fill_(1)
+                m.bias.data.zero_()
+            elif isinstance(m, nn.Linear):
+                n = m.weight.size(1)
+                m.weight.data.normal_(0, 0.01)
+                m.bias.data.zero_()
+
+def make_layers(cfg, batch_norm=False):
+    layers = []
+    in_channels = 3
+    for v in cfg:
+        if v == 'M':
+            layers += [nn.MaxPool2d(kernel_size=2, stride=2)]
+        else:
+            conv2d = nn.Conv2d(in_channels, v, kernel_size=3, padding=1)
+            if batch_norm:
+                layers += [conv2d, nn.BatchNorm2d(v), nn.ReLU(inplace=True)]
+            else:
+                layers += [conv2d, nn.ReLU(inplace=True)]
+            in_channels = v
+    return nn.Sequential(*layers)
+
+cfg = {
+    'A': [64, 'M', 128, 'M', 256, 256, 'M', 512, 512, 'M', 512, 512, 'M'],
+    'B': [64, 64, 'M', 128, 128, 'M', 256, 256,
+          'M', 512, 512, 'M', 512, 512, 'M'],
+    'D': [64, 64, 'M', 128, 128, 'M', 256, 256, 256,
+          'M', 512, 512, 512, 'M', 512, 512, 512, 'M'],
+    'E': [64, 64, 'M', 128, 128, 'M', 256, 256, 256, 256,
+          'M', 512, 512, 512, 512, 'M', 512, 512, 512, 512, 'M'],
+}
+
+def vgg11(**kwargs):
+    model = VGG(make_layers(cfg['A']), **kwargs)
+    return model
+
+def vgg11_bn(**kwargs):
+    model = VGG(make_layers(cfg['A'], batch_norm=True), **kwargs)
+    return model
+
+def vgg13(**kwargs):
+    model = VGG(make_layers(cfg['B']), **kwargs)
+    return model
+
+def vgg13_bn(**kwargs):
+    model = VGG(make_layers(cfg['B'], batch_norm=True), **kwargs)
+    return model
+
+def vgg16(**kwargs):
+    model = VGG(make_layers(cfg['D']), **kwargs)
+    return model
+
+def vgg16_bn(**kwargs):
+    model = VGG(make_layers(cfg['D'], batch_norm=True), **kwargs)
+    return model
+
+def vgg19(**kwargs):
+    model = VGG(make_layers(cfg['E']), **kwargs)
+    return model
+
+def vgg19_bn(**kwargs):
+    model = VGG(make_layers(cfg['E'], batch_norm=True), **kwargs)
+    return model
